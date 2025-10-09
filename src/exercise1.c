f4a869296ecaf36c012d0cfed3e08accf8c1cfca
Залесский Семён Сергеевич и Юкленчук Кирилл Андреевич ИП-515 <s@localhost.localdomain>
2025-10-10 01:04:20 +0700

    коммичу0

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char** argv) {
-    float m[10];
-    scanf("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f", &m[0], &m[1], &m[2],&m[3], &m[4], &m[5], &m[6], &m[7], &m[8], &m[9]);
-
-    printf("%f\n", (m[0]+m[1]+m[2]+m[3]+m[4]+m[5]+m[6]+m[7]+m[8]+m[9])/10);
+    double sum=0.0,num;
+    for (int i = 0; i < 10; i++){
+        scanf("%lf", &num);
+        sum+=num;
+    }
+    printf("%.2f\n", sum /10);
     return 0;
 }
